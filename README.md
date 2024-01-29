9264b08e68454caf103652c7f4411274aed3c998
urie-cruz <uriel.cruz@aulavirtual.umar.mx>
2024-01-29 16:42:01 -0600

    login y registro

--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 Example: CI/CD
 
 uriel
+
+Crear un entorno virtual
+    python -m venv .venv
+
+Activar entorno virtual:
+    .venv\Scripts\activate
+
+Desactivar entorno virtual
+    deactivate
+
+Instalar dependencias del proyecto
+    npm install
+
+
+Pasos para ejecutar el proyecto:
+
+    1. Crear un entorno virtual.
+    2. Activar el entorno virtual.
+    3. Instala las dependencias de python utilizando el comando: pip install -r "requirements.txt"
+    4. Instale las dependencias de node utilizando el comando: npm install
+    5. Ejecutar el archivo main.py con el comando: Python run.py
--- src/Server.py
+++ src/Server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from modules.auth.models.User import Base
+from database.db import db
 
 class Backend:
     def __init__(self, name, config, *blueprints):
@@ -6,6 +8,7 @@ class Backend:
         self.__app.config.from_object(config)
         self.register_error_handlers()
         self.register_blueprints(*blueprints)
+        self.register_extensions()
 
     def register_error_handlers(self):
         @self.__app.errorhandler(404)
@@ -15,6 +18,11 @@ class Backend:
     def register_blueprints(self, *blueprints):
         for blueprint in blueprints:
             self.__app.register_blueprint(blueprint)
+
+    def register_extensions(self):
+        db.init_app(self.__app)
+        with self.__app.app_context():
+            Base.metadata.create_all(bind=db.engine)
 
     def run(self, *args, **kwargs):
         self.__app.run(*args, **kwargs)
